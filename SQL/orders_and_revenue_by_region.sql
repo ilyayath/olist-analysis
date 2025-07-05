8cda6647f379fdb41616ee45cd0f8cc8c45e71cd
ilyayath <143421847+ilyayath@users.noreply.github.com>
2025-07-05 15:43:13 +0300

    Update orders_and_revenue_by_region.sql

--- SQL/orders_and_revenue_by_region.sql
+++ SQL/orders_and_revenue_by_region.sql
@@ -1,9 +1,13 @@
 SELECT
-  c.customer_state,
-  COUNT(DISTINCT o.order_id) AS total_orders,
-  ROUND(SUM(oi.price)) AS total_revenue
+    c.customer_state,
+    pt.product_category_name_english,
+    COUNT(DISTINCT o.order_id) AS total_orders,
+    ROUND(SUM(oi.price)) AS total_revenue
 FROM olist_customers_dataset AS c
 JOIN olist_orders_dataset AS o ON c.customer_id = o.customer_id
 JOIN olist_order_items_dataset AS oi ON o.order_id = oi.order_id
-GROUP BY c.customer_state
+JOIN olist_products_dataset AS p ON oi.product_id = p.product_id
+JOIN product_category_name_translation AS pt ON p.product_category_name = pt.product_category_name
+WHERE o.order_status = 'delivered'
+GROUP BY c.customer_state, pt.product_category_name_english
 ORDER BY total_orders DESC;
